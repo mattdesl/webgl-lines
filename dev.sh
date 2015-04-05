d07485a39fb4b44cbfc11f4719d4ad0261128707
mattdesl <dave.des@gmail.com>
2015-04-05 13:28:56 -0400

    use local budo

--- dev.sh
+++ dev.sh
@@ -2,4 +2,4 @@
 # 2. run watchify server with live reload
 rm -rf $1/bundle.js $1/bundle.min.js \
   && cp base/dev.html $1/index.html \
-  && budo $1/index.js -o $1/bundle.js -v -d --live | garnish -v
+  && ./node_modules/.bin/budo $1/index.js -o $1/bundle.js -v -d --live | garnish -v
